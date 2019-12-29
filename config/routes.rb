edf25d6e68139e51e949f86b7ff120a074a7ab4c
Melanie Locke <melanie.a.locke@gmail.com>
2019-12-29 00:31:44 +0000

    Get rid of portfolio and bookrecs pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,4 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home_pages#index'
     resources :about, only: [:index]
-    resources :portfolio, only: [:index]
-    resources :portfolio, only: [:index]
-    resources :book_recs, only: [ :index]
 end
